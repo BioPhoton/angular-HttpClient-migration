bd0f9fd33273e65fcae6ff0402ab9bb7fe91614d
Michael Hladky <michael@hladky.at>
2017-08-26 23:17:18 +0200

    feat(Implement a  http intercepter): Provide it under the CoreModule providers
    Related to #2

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,7 @@ import {ModuleWithProviders, NgModule} from '@angular/core';
 import {HttpModule} from '@angular/http';
 import {FlightService} from './services/flight.service';
 import {UpgradedFlightService} from './services/upgraded-flight.service';
+import {GLOBAL_INTERCEPTER_PROVIDER} from './global.intercepter';
 
 @NgModule({
   imports: [
@@ -18,7 +19,8 @@ export class CoreModule {
     return {
       ngModule: CoreModule,
       providers: [
-        {provide: FlightService, useClass: UpgradedFlightService}
+        {provide: FlightService, useClass: UpgradedFlightService},
+        GLOBAL_INTERCEPTER_PROVIDER
       ]
     }
   }
