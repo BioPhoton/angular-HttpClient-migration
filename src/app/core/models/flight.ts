80a2d69bd4276f05033aaf7ffa9f74b34e91c333
Michael Hladky <michael@hladky.at>
2017-08-27 01:03:57 +0200

    adpot interface

--- src/app/core/models/flight.ts
+++ src/app/core/models/flight.ts
@@ -2,5 +2,5 @@ export interface Flight {
   id: number; // int+double
   from: string;
   to: string;
-  date: Date; // ISO-Format: 2017-12-24T17:00:00.000+01:00
+  date: string; // ISO-Format: 2017-12-24T17:00:00.000+01:00
 }
