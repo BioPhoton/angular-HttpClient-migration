6bdd02a609b00c7b4af3411f7e083fff0bb60e87
Michael Hladky <michael@hladky.at>
2017-08-26 21:07:12 +0200

    fix(upgrade FlightService): change URLSearchParams to HttpParams and remove map to json
    Related to #1

--- src/app/core/services/upgraded-flight.service.ts
+++ src/app/core/services/upgraded-flight.service.ts
@@ -1,18 +1,11 @@
+import {HttpClient, HttpErrorResponse, HttpParams} from '@angular/common/http';
 import {Injectable} from '@angular/core';
-
-import {
-  Headers,
-  Http,
-  RequestOptionsArgs,
-  URLSearchParams
-} from '@angular/http';
+import {Observable} from 'rxjs/Rx';
 
 import 'rxjs/Rx';
-import {Observable} from 'rxjs/Rx';
 
 import {environment} from '../../../environments/environment';
 import {Flight} from '../models/Flight';
-import {HttpErrorResponse} from '@angular/common/http';
 import {IFlightService} from './flight.service.interface';
 
 @Injectable()
@@ -21,51 +14,38 @@ export class UpgradedFlightService implements IFlightService {
   private baseUrl: string
   private resourceName = 'flight'
 
-  constructor(private http: Http) {
+  constructor(private http: HttpClient) {
     this.baseUrl = [environment.baseUrl, this.resourceName].join('/')
   }
 
   findById(id: string): Observable<Flight> {
+    const reqObj = { params: null};
+    const params = new HttpParams().set('id', id)
+    // Wont work!! => params.set('id', id)
+    reqObj.params = params
 
-    const headers = new Headers()
-    headers.set('Accept', 'text/json')
-
-    const search = new URLSearchParams()
-    search.set('id', id)
-
-    return this
-      .http
-      .get(this.baseUrl, {headers, search})
-      .map(resp => resp.json())
-
+    return this.http
+      .get(this.baseUrl, reqObj)
+      .catch(error => Observable.throw(error.json()))
   }
 
   find(from: string, to: string): Observable<Flight[]> {
-    const headers = new Headers()
-    headers.set('Accept', 'application/json')
-
-    const search = new URLSearchParams()
-    search.set('from', from)
-    search.set('to', to)
-    const reqParams: RequestOptionsArgs = {headers, search}
+    const reqObj = {
+      params: new HttpParams()
+        .set('from', from || '')
+        .set('to', to || '')
+    };
 
     return this
       .http
-      .get(this.baseUrl, reqParams)
-      .map(response => (response) ? response.json() : [])
+      .get(this.baseUrl, reqObj)
       .catch(error => Observable.throw(error.json()))
   }
 
   create(flight: Flight): Observable<Flight> {
-    const headers = new Headers()
-    headers.set('Accept', 'application/json')
-
-    const reqParams: RequestOptionsArgs = {headers}
-
     return this
       .http
-      .post(this.baseUrl, flight, reqParams)
-      .map((response) => (response) ? response.json() : [])
+      .post(this.baseUrl, flight)
       .catch((e: HttpErrorResponse) => {
         let errMsg = 'Client Error'
         if (e.error instanceof Error) {
@@ -74,7 +54,5 @@ export class UpgradedFlightService implements IFlightService {
         return Observable.throw({message: errMsg})
       })
   }
-
-
 
 }
